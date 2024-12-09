913579581e3d8f263da6ade1a077468a4f636b27
Randy <rebucasrandy1986@gmail.com>
2024-12-09 20:22:49 +0800

    update index route

--- controllers/index.js
+++ controllers/index.js
@@ -1,17 +1,5 @@
 
 exports.index = async (req, res) => {
-    res.json({ message: 'Hello World' });
-}
-
-exports.health = async (req, res) => {
-    res.json({ message: 'Hello World' });
-}
-
-exports.notFound = async (req, res) => {
-    res.status(404).json({ message: 'Not Found' });
-}
-
-exports.error = async (req, res) => {
-    res.status(500).json({ message: 'Internal Server Error' });
+    res.json({ message: 'NPC Logto API', version: '1.0.0' });
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { index, health, notFound, error } = require('../controllers/index');
+const { index } = require("../controllers/index");
 
 /* GET home page. */
-router.get('/', index);
-router.get('/health', health);
-router.get('/not-found', notFound);
-router.get('/error', error);
+router.get("/", index);
 
 module.exports = router;
